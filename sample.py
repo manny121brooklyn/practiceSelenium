6f7c99df9eb9ad490fa90da8d8a846f556a8175b
manny121 <manny121@gmail.com>
2022-12-18 00:17:10 -0500

    added a new file

--- sample.py
+++ sample.py
@@ -24,6 +24,22 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+# url = 'https://www.google.com'
+# search_box = 'q'
+# search_string = 'Python for beginners'
+# button = 'btnK'
+#
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.get(url)
+# search = driver.find_element(By.NAME, search_box)
+# search.send_keys(search_string)
+# btn = driver.find_element(By.NAME, button)
+# driver.execute_script('arguments[0].click();', btn)
+# driver.quit()
+#
+# driver.get('https://www.youtube.com')
+
+
 url = 'https://www.google.com'
 search_box = 'q'
 search_string = 'Python for beginners'
@@ -35,6 +51,7 @@ search = driver.find_element(By.NAME, search_box)
 search.send_keys(search_string)
 btn = driver.find_element(By.NAME, button)
 driver.execute_script('arguments[0].click();', btn)
+
 driver.quit()
 
 driver.get('https://www.youtube.com')
