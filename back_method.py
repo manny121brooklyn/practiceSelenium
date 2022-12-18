3cd154d028103fa9058b014a789370c07512fa8c
manny121 <manny121@gmail.com>
2022-12-17 23:35:03 -0500

    added a new file

---
+++ back_method.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.google.com')
+time.sleep(2)
+driver.get('https://www.python.org')
+time.sleep(2)
+driver.refresh()
+time.sleep(1)
+driver.back()
+
+driver.close()
