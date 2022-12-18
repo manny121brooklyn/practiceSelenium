a57c7471459ab194369e6f71ba182213da223ee7
manny121 <manny121@gmail.com>
2022-12-18 00:04:31 -0500

    added a new file

--- implicitly_wait.py
+++ implicitly_wait.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-url =
+url = 'https://www.google.com'
 
 # create an object
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -11,3 +11,12 @@ driver.maximize_window()
 driver.get(url)
 driver.implicitly_wait(10)
 
+driver.get(url)
+element = driver.find_element(By.NAME, 'q')
+element.send_keys('bdd approach')
+
+btn = driver.find_element(By.NAME, 'btnK')
+# use javascript executor
+driver.execute_script('arguments[0].click()',btn)
+
+driver.close()
