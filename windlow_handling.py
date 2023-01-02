393cbb67180369d2df006d9b33d8002ecb8f1ff1
manny121 <manny121@gmail.com>
2023-01-02 18:05:47 -0500

    added a new file

--- windlow_handling.py
+++ windlow_handling.py
@@ -16,11 +16,14 @@ driver.find_element(By.LINK_TEXT, 'Click Here').click()
 handles = driver.window_handles  #find all window handlers
 print(handles)
 driver.switch_to.window(handles[0]) #switch to parent window
+print(driver.current_window_handle)
 time.sleep(3)
 driver.switch_to.window(handles[1]) #swithc to child window
+print(driver.current_window_handle)
 print(driver.find_element(By.TAG_NAME, 'h3').text) #assert right window
 time.sleep(2)
 parent = driver.switch_to.window(handles[0]) #switch back to child window
+print(driver.current_window_handle)
 assert driver.title == 'The Internet'
 print(driver.title)
 
